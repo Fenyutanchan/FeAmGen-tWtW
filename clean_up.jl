9481896d25f0daceb64e89aa9d3769c65645cb8f
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2023-02-26 11:52:51 +0800

    add functional for getting visual tex files.

--- clean_up.jl
+++ clean_up.jl
@@ -1,1 +1,3 @@
 rm("Wplus_t_TO_Wplus_t", force=true, recursive=true)
+rm("with_Fermion_loops", force=true, recursive=true)
+rm("with_light_quark_loops", force=true, recursive=true)
--- get_with_Fermion_loops.jl
+++ get_with_Fermion_loops.jl
@@ -2,7 +2,7 @@ using   JLD2
 
 root_dir        =   dirname(@__FILE__)
 process_name    =   "Wplus_t_TO_Wplus_t"
-archive_dir     =   joinpath(root_dir, "amp_files_with_Fermion_loops")
+archive_dir     =   joinpath(root_dir, "with_Fermion_loops")
 loops           =   0:3
 
 # amplitudes_with_Fermion_loops   =   Vector{String}[]
@@ -36,6 +36,29 @@ function main()::Int
                         joinpath(
                             archive_dir,
                             "$(n_loop)Loop_" * (last ∘ splitdir)(amp_file)
+                        );
+                        force=true
+                    )
+                    visual_tex_file =   begin
+                        amp_dir_name, amp_file_name =   splitdir(amp_file)
+
+                        tex_dir_name    =   replace(
+                            amp_dir_name,
+                            "amplitudes" => "visuals"
+                        )
+                        tex_file_name   =   replace(
+                            amp_file_name,
+                            "amplitude" => "visual",
+                            "jld2" => "tex"
+                        )
+
+                        joinpath(tex_dir_name, tex_file_name)
+                    end
+                    cp(
+                        visual_tex_file,
+                        joinpath(
+                            archive_dir,
+                            "$(n_loop)Loop_" * (last ∘ splitdir)(visual_tex_file)
                         );
                         force=true
                     )
